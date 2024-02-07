da64a8dca6a8c25f2e2890539762c1b2c8bdf134
Kenneth Lloyd Boller <122646869+Fll0yd@users.noreply.github.com>
2024-02-07 13:52:16 -0600

    Delete New folder directory

--- New folder/SNES - The Legend of Zelda A Link to the Past - Dark World.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="SNES - The Legend of Zelda A Link to the Past - Dark World" tilewidth="32" tileheight="32" tilecount="17424" columns="132">
- <image source="SNES - The Legend of Zelda A Link to the Past - Dark World.png" width="4240" height="4240"/>
-</tileset>
